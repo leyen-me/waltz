c8ef43d3ac3893a2e1c36c0cbd8ebf8b54b4a75b
DIFFFFFFT <672228275@qq.com>
2024-04-24 16:01:23 +0800

    change mysql sh

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,5 +1,5 @@
 docker run -idt -p 3306:3306 --privileged=true \
 -e TZ=Asia/Shanghai \
--e MYSQL_DATABASE=open-nuxt-blog-mysql \
--e MYSQL_ROOT_PASSWORD=pdBGKGjRyX3Jb2Hn \
---name open-nuxt-blog-mysql mysql:8.0.20
+-e MYSQL_DATABASE=open-nuxt-blog \
+-e MYSQL_ROOT_PASSWORD=JGhQ83axm5ydtQEnX8B3RgtqnFIY6U3+TO5VMMVyLxA= \
+--name open-nuxt-blog mysql:8.0.20
