592f374d9abd3d071c33b6ebf631fb21ea5cbffc
DIFFFFFFT <672228275@qq.com>
2024-06-20 09:02:39 +0800

    feat(progress): add detail progress (#26)
    fix #26

--- app.vue
+++ app.vue
@@ -1,13 +1,25 @@
 <template>
   <NuxtLayout>
     <div class="w-full h-full">
+      <BaseLoadingIndicator v-if="!showLoading" />
+
+      <!-- 在这里 -->
       <NuxtPage />
     </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-import "@/utils/banner"
+import "@/utils/banner";
+import BaseLoadingIndicator from "@/components/BaseLoadingIndicator";
+
+const route = useRoute()
+
+// 二级以上路由不显示 
+const showLoading = computed(()=>{
+  const routeName = route.name?.toString() as string
+  return routeName.startsWith("admin") || routeName.startsWith("app")
+})
 
 onMounted(() => {
   document.documentElement.setAttribute("theme-mode", "dark");
---
+++ components/BaseLoadingIndicator/index.ts
@@ -0,0 +1,63 @@
+import { defineComponent, h } from 'vue'
+import { useLoadingIndicator } from '#app/composables/loading-indicator'
+
+export default defineComponent({
+  name: 'NuxtLoadingIndicator',
+  props: {
+    throttle: {
+      type: Number,
+      default: 200,
+    },
+    duration: {
+      type: Number,
+      default: 2000,
+    },
+    height: {
+      type: Number,
+      default: 3,
+    },
+    color: {
+      type: [String, Boolean],
+      default: 'repeating-linear-gradient(to right,#e88d15 0%,#e88d15 50%,#e88d15 100%)',
+    },
+    errorColor: {
+      type: String,
+      default: 'repeating-linear-gradient(to right,#f87171 0%,#ef4444 100%)',
+    },
+    estimatedProgress: {
+      type: Function as unknown as () => (duration: number, elapsed: number) => number,
+      required: false,
+    },
+  },
+  setup (props, { slots, expose }) {
+    const { progress, isLoading, error, start, finish, clear } = useLoadingIndicator({
+      duration: props.duration,
+      throttle: props.throttle,
+      estimatedProgress: props.estimatedProgress,
+    })
+
+    expose({
+      progress, isLoading, error, start, finish, clear,
+    })
+
+    return () => h('div', {
+      class: 'nuxt-loading-indicator',
+      style: {
+        position: 'fixed',
+        top: 0,
+        right: 0,
+        left: 0,
+        pointerEvents: 'none',
+        width: 'auto',
+        height: `${props.height}px`,
+        opacity: isLoading.value ? 1 : 0,
+        background: error ? props.errorColor : props.color || undefined,
+        backgroundSize: `${(100 / progress.value) * 100}% auto`,
+        transform: `scaleX(${progress.value}%)`,
+        transformOrigin: 'left',
+        transition: 'transform 0.1s, height 0.4s, opacity 0.4s',
+        zIndex: 999999,
+      },
+    }, slots)
+  },
+})
